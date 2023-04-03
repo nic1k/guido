3124565b8d00ca25b3d2e107ef521550328f1d83
nic1k <nic1k.88@gmail.com>
2023-04-03 09:28:43 +0200

    HTML: Frontpage

--- bedrock/web/app/themes/sage/index.php
+++ bedrock/web/app/themes/sage/index.php
@@ -10,9 +10,7 @@
     <?php wp_body_open(); ?>
     <?php do_action('get_header'); ?>
 
-    <div id="app">
-      <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
-    </div>
+    <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
 
     <?php do_action('get_footer'); ?>
     <?php wp_footer(); ?>
--- bedrock/web/app/themes/sage/resources/views/layouts/app.blade.php
+++ bedrock/web/app/themes/sage/resources/views/layouts/app.blade.php
@@ -1,3 +1,4 @@
+<x-svg-open-body/>
 @include('sections.header')
 @yield('content')
 @include('sections.footer')
