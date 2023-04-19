7f9ba20be83e2fdbf32356b886ff8e5ba0eec27c
nic1k <nic1k.88@gmail.com>
2023-04-19 10:35:14 +0200

    Disable gutenberg in page post type

--- bedrock/web/app/themes/sage/app/filters.php
+++ bedrock/web/app/themes/sage/app/filters.php
@@ -14,3 +14,13 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/**
+ * Deactive Gutenberg on Page post type
+ *
+ * @return object
+ */
+add_filter( 'use_block_editor_for_post_type', function( $current_status, $post_type ) {
+    if ( 'page' === $post_type ) return false;
+    return $current_status;
+}, 10, 2 );
